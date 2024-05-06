2420be397a921ffd2bccd5c7a885984ae78e57d7
Isabella Solarte <isabsolarte@.edu.co>
2024-05-06 15:15:53 -0500

    Se creó el proyecto

---
+++ src/main/java/co/edu/unicauca/segundotaller/asst/asst_segundo_taller/AsstSegundoTallerApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.segundotaller.asst.asst_segundo_taller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsstSegundoTallerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsstSegundoTallerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unicauca/segundotaller/asst/asst_segundo_taller/AsstSegundoTallerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.segundotaller.asst.asst_segundo_taller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsstSegundoTallerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
