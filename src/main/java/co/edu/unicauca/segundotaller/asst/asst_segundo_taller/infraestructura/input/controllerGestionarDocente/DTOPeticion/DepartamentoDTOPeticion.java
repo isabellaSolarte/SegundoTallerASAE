5c6e3843145712c75939cc4fca4a14ce9e0a1261
IsabellaSolarte <isabsolarte@unicauca.edu.co>
2024-05-13 16:56:25 -0500

    Prueba de subida

---
+++ src/main/java/co/edu/unicauca/segundotaller/asst/asst_segundo_taller/infraestructura/input/controllerGestionarDocente/DTOPeticion/DepartamentoDTOPeticion.java
@@ -0,0 +1,22 @@
+package co.edu.unicauca.segundotaller.asst.asst_segundo_taller.infraestructura.input.controllerGestionarDocente.DTOPeticion;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+public class DepartamentoDTOPeticion {
+     @NotNull(message = "{departamento.iddepartamento.emply}")
+    private int iddepartamento;
+
+    @NotNull(message = "{departamento.nombre.emply}")
+    @Size(min = 5, max = 25, message = "{departamento.nombre.size}")
+    private String nombre;
+
+    @NotNull(message = "{departamento.descripcion.emply}")
+    @Size(min = 5, max = 35, message = "{departamento.descripcion.size}")
+    private String descripcion;
+}
